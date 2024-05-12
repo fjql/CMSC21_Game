17e8550d3ae0b3d4cdd482b87d93f75018375ee7
fjql <98269704+fjql@users.noreply.github.com>
2024-05-13 07:55:23 +0800

    Fixed Make.ps1 so that the program gets called only after it is compiled (?)

--- Make.ps1
+++ Make.ps1
@@ -5,18 +5,16 @@ if (-not (Test-Path -Path "bin")) {
 
 $compiler = "clang++"
 if ($Args -contains "g++") {
-    $compiler = g++
+    $compiler = "g++"
 }
 
-$compiler_args = "./src/*.cpp -o ./bin/Game.exe -std=c++2b -Wall -Wextra -O1"
-
 if ((Get-Command "clang++.exe") -or (Get-Command "g++.exe")) {
-    Start-Process -FilePath $compiler -ArgumentList $compiler_args -NoNewWindow
+    & $compiler ./src/*.cpp -o ./bin/Game.exe -std=c++2b -Wall -Wextra -O1
+
+    if ($Args -contains "run") {
+        bin/Game.exe
+    }
 } else {
     Write-Host "Can't find compiler." -ForegroundColor Red
     Return
-}
-
-if ($Args -contains "run") {
-    bin/Game.exe
 }
